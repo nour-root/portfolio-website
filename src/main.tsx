8d1d2e99d1f6afb22d30bcad91b48b6d1cd83fbb
Nour Mahmoud <nour.m.mahmod@gmail.com>
2025-07-31 08:07:32 +0300

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
